9ae9c1e82ffff90198aa75882487bfdc5afb692a
Cheesmaker <83553936+Cheesmaker@users.noreply.github.com>
2025-03-17 22:21:05 +0100

    Update M2Emulator.cpp
    send ROM name
    prepare for futur RPM and speed value

--- Game Files/M2Emulator.cpp
+++ Game Files/M2Emulator.cpp
@@ -17,27 +17,84 @@ along with Output Blaster.If not, see < https://www.gnu.org/licenses/>.*/
 static int detectedGameId = 0;
 static int WindowsLoop()
 {
-	INT_PTR Rambase = helpers->ReadIntPtr(0x1AA888, true);
-	INT_PTR RambaseA = helpers->ReadIntPtr(Rambase + 0x100, false);
-	UINT8 data = helpers->ReadByte(RambaseA + 0x824, false);
-
-	Outputs->SetValue(OutputLampStart, !!(data & 0x04));
-	Outputs->SetValue(OutputLampLeader, !!(data & 0x80));
-	Outputs->SetValue(OutputLampView1, !!(data & 0x08));
-	Outputs->SetValue(OutputLampView2, !!(data & 0x10));
-	Outputs->SetValue(OutputLampView3, !!(data & 0x20));
-	Outputs->SetValue(OutputLampView4, !!(data & 0x40));
-	Outputs->SetValue(OutputRawLamps, data);
-
-	if (detectedGameId == 10) {
-		//do some custom stuff for this game
-	}
-	if (detectedGameId == 12)
+	/*INT_PTR data1 = helpers->ReadIntPtr(0x174CF0, true);
+	Outputs->SetValue(OutputLampStart, !!(data1 & 0x04));
+	Outputs->SetValue(OutputLampLeader, !!(data1 & 0x80));
+	Outputs->SetValue(OutputLampView1, !!(data1 & 0x08));
+	Outputs->SetValue(OutputLampView2, !!(data1 & 0x10));
+	Outputs->SetValue(OutputLampView3, !!(data1 & 0x20));
+	Outputs->SetValue(OutputLampView4, !!(data1 & 0x40));*/
+	
+	/*sega rally*/
+	if ((detectedGameId == 1) || (detectedGameId == 14) || (detectedGameId == 15))
 	{
-		//do some more custom stuff for this game :)
-		//And so on...
+		INT_PTR data1 = helpers->ReadIntPtr(0x174CF0, true);
+		Outputs->SetValue(OutputLampStart, !!(data1 & 0x04));
+		Outputs->SetValue(OutputLampLeader, !!(data1 & 0x80));
+		Outputs->SetValue(OutputLampView1, !!(data1 & 0x08));
+		Outputs->SetValue(OutputLampView2, !!(data1 & 0x10));
+		Outputs->SetValue(OutputLampView3, !!(data1 & 0x20));
+		Outputs->SetValue(OutputLampView4, !!(data1 & 0x40));
 	}
+	/*stcc*/
+	else if ((detectedGameId == 4) || (detectedGameId == 18) || (detectedGameId == 19))
+	{
+		INT_PTR data2 = helpers->ReadIntPtr(0x174CF0, true);
+		Outputs->SetValue(OutputLampStart, !!(data2 & 0x04));
+		Outputs->SetValue(OutputLampLeader, !!(data2 & 0x80));
+		Outputs->SetValue(OutputLampView1, !!(data2 & 0x08));
+		Outputs->SetValue(OutputLampView2, !!(data2 & 0x10));
+		Outputs->SetValue(OutputLampView3, !!(data2 & 0x20));
+		Outputs->SetValue(OutputLampView4, !!(data2 & 0x40));
+	}
+	/*daytona*/
+	else if ((detectedGameId == 2) || (detectedGameId == 7) || (detectedGameId == 8) || (detectedGameId == 9) || (detectedGameId == 10) || (detectedGameId == 11) || (detectedGameId == 12) || (detectedGameId == 13))
+	{
+		INT_PTR Rambase = helpers->ReadIntPtr(0x1AA888, true);
+		INT_PTR RambaseA = helpers->ReadIntPtr(Rambase + 0x100, false);
+		UINT8 data = helpers->ReadByte(RambaseA + 0x824, false);
 
+		Outputs->SetValue(OutputLampStart, !!(data & 0x04));
+		Outputs->SetValue(OutputLampLeader, !!(data & 0x80));
+		Outputs->SetValue(OutputLampView1, !!(data & 0x08));
+		Outputs->SetValue(OutputLampView2, !!(data & 0x10));
+		Outputs->SetValue(OutputLampView3, !!(data & 0x20));
+		Outputs->SetValue(OutputLampView4, !!(data & 0x40));
+		Outputs->SetValue(OutputRawLamps, data);
+	}
+	/*indy500*/
+	else if ((detectedGameId == 3) || (detectedGameId == 16) || (detectedGameId == 17))
+	{
+		INT_PTR data2 = helpers->ReadIntPtr(0x174CF0, true);
+		Outputs->SetValue(OutputLampStart, !!(data2 & 0x04));
+		Outputs->SetValue(OutputLampLeader, !!(data2 & 0x80));
+		Outputs->SetValue(OutputLampView1, !!(data2 & 0x08));
+		Outputs->SetValue(OutputLampView2, !!(data2 & 0x10));
+		Outputs->SetValue(OutputLampView3, !!(data2 & 0x20));
+		Outputs->SetValue(OutputLampView4, !!(data2 & 0x40));
+	}
+	/*overrev*/
+	else if ((detectedGameId == 20) || (detectedGameId == 5))
+	{
+		INT_PTR data2 = helpers->ReadIntPtr(0x174CF0, true);
+		Outputs->SetValue(OutputLampStart, !!(data2 & 0x04));
+		Outputs->SetValue(OutputLampLeader, !!(data2 & 0x80));
+		Outputs->SetValue(OutputLampView1, !!(data2 & 0x08));
+		Outputs->SetValue(OutputLampView2, !!(data2 & 0x10));
+		Outputs->SetValue(OutputLampView3, !!(data2 & 0x20));
+		Outputs->SetValue(OutputLampView4, !!(data2 & 0x40));
+	}
+	/*supergt24*/
+	else if (detectedGameId == 6)
+	{
+		INT_PTR data2 = helpers->ReadIntPtr(0x174CF0, true);
+		Outputs->SetValue(OutputLampStart, !!(data2 & 0x04));
+		Outputs->SetValue(OutputLampLeader, !!(data2 & 0x80));
+		Outputs->SetValue(OutputLampView1, !!(data2 & 0x08));
+		Outputs->SetValue(OutputLampView2, !!(data2 & 0x10));
+		Outputs->SetValue(OutputLampView3, !!(data2 & 0x20));
+		Outputs->SetValue(OutputLampView4, !!(data2 & 0x40));
+	}
 	return 0;
 }
 
@@ -53,16 +110,16 @@ static DWORD WINAPI OutputsAreGo(LPVOID lpParam)
 
 void M2Emulator::OutputsGameLoop()
 {
+	
 	if (!init)
 	{
-		Outputs = new CWinOutputs();
-		m_game.name = "M2 Emulator";
+		/*Outputs = new CWinOutputs();
+		m_game.name = "M2 Emulator"; 
 		Outputs->SetGame(m_game);
 		Outputs->Initialize();
-		Outputs->Attached();
+		Outputs->Attached();*/
 
-
-		if(FindWindowA(0, ("Sega Rally Championship"))) detectedGameId = 1;
+		if (FindWindowA(0, ("Sega Rally Championship"))) detectedGameId = 1;
 		else if (FindWindowA(0, ("Daytona USA")))  detectedGameId = 2;
 		else if (FindWindowA(0, ("Indianapolis 500 (Rev A, Deluxe)")))  detectedGameId = 3;
 		else if (FindWindowA(0, ("Sega Touring Car Championship (Rev A)")))  detectedGameId = 4;
@@ -82,14 +139,108 @@ void M2Emulator::OutputsGameLoop()
 		else if (FindWindowA(0, ("Sega Touring Car Championship")))  detectedGameId = 18;
 		else if (FindWindowA(0, ("Sega Touring Car Championship (Rev B)")))  detectedGameId = 19;
 		else if (FindWindowA(0, ("Over Rev (Model 2B)")))  detectedGameId = 20;
-
-
-		CreateThread(NULL, 0, OutputsAreGo, NULL, 0, NULL);
-		while (GetMessage(&Msg1, NULL, NULL, 0))
+		
+		if (detectedGameId == 1)
 		{
-			TranslateMessage(&Msg1);
-			DispatchMessage(&Msg1);
+			m_game.name = "Sega Rally Championship";
 		}
-		init = true;
+		else if (detectedGameId == 2)
+		{
+			m_game.name = "Daytona USA";
+		}
+		else if (detectedGameId == 3)
+		{
+			m_game.name = "Indianapolis 500 (Rev A, Deluxe)";
+		}
+		else if (detectedGameId == 4)
+		{
+			m_game.name = "Sega Touring Car Championship (Rev A)";
+		}
+		else if (detectedGameId == 5)
+		{
+			m_game.name = "Over Rev";
+		}
+		else if (detectedGameId == 6)
+		{
+			m_game.name = "Super GT 24h";
+		}
+		else if (detectedGameId == 7)
+		{
+			m_game.name = "Daytona USA '93 Edition";
+		}
+		else if (detectedGameId == 8)
+		{
+			m_game.name = "Daytona USA (Saturn Ads)";
+		}
+		else if (detectedGameId == 9)
+		{
+			m_game.name = "Daytona USA Special Edition";
+		}
+		else if (detectedGameId == 10)
+		{
+			m_game.name = "Daytona USA Turbo";
+		}
+		else if (detectedGameId == 11)
+		{
+			m_game.name = "Daytona USA Turbo (Rev A)";
+		}
+		else if (detectedGameId == 12)
+		{
+			m_game.name = "Daytona USA: GTX 2004";
+		}
+		else if (detectedGameId == 13)
+		{
+			m_game.name = "Daytona USA: To The Maxx";
+		}
+		else if (detectedGameId == 14)
+		{
+			m_game.name = "Sega Rally Championship (Rev B)";
+		}
+		else if (detectedGameId == 15)
+		{
+			m_game.name = "Sega Rally Pro Drivin'";
+		}
+		else if (detectedGameId == 16)
+		{
+			m_game.name = "Indianapolis 500 (Rev A, Twin, Newer rev)";
+		}
+		else if (detectedGameId == 17)
+		{
+			m_game.name = "Indianapolis 500 (Rev A, Twin, Older rev)";
+		}
+		else if (detectedGameId == 18)
+		{
+			m_game.name = "Sega Touring Car Championship";
+		}
+		else if (detectedGameId == 19)
+		{
+			m_game.name = "Sega Touring Car Championship (Rev B)";
+		}
+		else if (detectedGameId == 20)
+		{
+			m_game.name = "Over Rev (Model 2B)";
+		}
+		
+		if (detectedGameId != 0)
+		{
+			Outputs = new CWinOutputs();
+			Outputs->SetGame(m_game);
+			Outputs->Initialize();
+			Outputs->Attached();
+
+
+			CreateThread(NULL, 0, OutputsAreGo, NULL, 0, NULL);
+			while (GetMessage(&Msg1, NULL, NULL, 0))
+			{
+				TranslateMessage(&Msg1);
+				DispatchMessage(&Msg1);
+			}
+
+			init = true;
+		}
+		else if(detectedGameId == 0)
+		{
+			init = false;
+		}
 	}
 }
